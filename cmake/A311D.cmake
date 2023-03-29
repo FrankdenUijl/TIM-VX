c02934ea6d140db8791971ee6d73ccef49dc2da5
FrankdenUijl <frankdenuijl@gmail.com>
2023-03-29 13:57:50 +0200

    Update A311D.cmake
    Update cmake

--- cmake/A311D.cmake
+++ cmake/A311D.cmake
@@ -7,6 +7,9 @@ endif()
 set(ANDROID_NDK_PATH $ENV{ANDROID_NDK})
 set(CMAKE_TOOLCHAIN_FILE ${ANDROID_NDK_PATH}/build/cmake/android.toolchain.cmake)
 
+set(ANDROID_ABI "armeabi-v7a")
+set(ANDROID_PLATFORM "android-28")
+
 include(${CMAKE_TOOLCHAIN_FILE})
 
 message("Downloading A311D SDK ...")
@@ -14,7 +17,8 @@ file(DOWNLOAD "https://github.com/FrankdenUijl/TIM-VX/releases/download/Binaries
     ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3.tgz
     SHOW_PROGRESS)
 execute_process(COMMAND
-    tar xf ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3.tgz)
+    tar xf ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3.tgz
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
 
 set(OVXDRV_INCLUDE_DIRS)
 list(APPEND OVXDRV_INCLUDE_DIRS
