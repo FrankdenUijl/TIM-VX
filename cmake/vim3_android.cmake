70d2c7b094444faa82608d0acb729dac1186ceb4
FrankdenUijl <frankdenuijl@gmail.com>
2023-03-29 14:30:05 +0200

    Update vim3_android.cmake
    update libs

--- cmake/vim3_android.cmake
+++ cmake/vim3_android.cmake
@@ -5,26 +5,25 @@ set(SDK_URL "https://github.com/VeriSilicon/TIM-VX/releases/download/v1.1.28/arm
 set(TARGET "arm_android9_A311D_6.4.3")
 
 message("Downloading android vim3 SDK ...")
-file(DOWNLOAD ${SDK_URL}
-    ${PROJECT_BINARY_DIR}/${TARGET}.tgz
-    EXPECTED_MD5 "92186fa61db7919aeb166d8823c1c214"
+file(DOWNLOAD "https://github.com/FrankdenUijl/TIM-VX/releases/download/Binaries/arm_A311D_6.4.4.3.tgz"
+    ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3.tgz
     SHOW_PROGRESS)
 execute_process(COMMAND
-    tar xf ${PROJECT_BINARY_DIR}/${TARGET}.tgz)
+    tar xf ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3.tgz
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
 
 set(OVXDRV_INCLUDE_DIRS)
 list(APPEND OVXDRV_INCLUDE_DIRS
-    ${PROJECT_BINARY_DIR}/${TARGET}/include
-    ${PROJECT_BINARY_DIR}/${TARGET}/include/CL)
+    ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3/include
+    ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3/include/CL)
 
 set(OVXDRV_LIBRARIES)
 list(APPEND OVXDRV_LIBRARIES
-    ${PROJECT_BINARY_DIR}/${TARGET}/lib/libCLC.so
-    ${PROJECT_BINARY_DIR}/${TARGET}/lib/libGAL.so
-    ${PROJECT_BINARY_DIR}/${TARGET}/lib/libOpenVX.so
-    ${PROJECT_BINARY_DIR}/${TARGET}/lib/libOpenVXU.so
-    ${PROJECT_BINARY_DIR}/${TARGET}/lib/libVSC.so
-    ${PROJECT_BINARY_DIR}/${TARGET}/lib/libarchmodelSw.so
-    ${PROJECT_BINARY_DIR}/${TARGET}/lib/libNNArchPerf.so)
+    ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3/lib/libGAL.so
+    ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3/lib/libOpenVX.so
+    ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3/lib/libOpenVXU.so
+    ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3/lib/libVSC.so
+    ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3/lib/libarchmodelSw.so
+    ${PROJECT_BINARY_DIR}/arm_A311D_6.4.4.3/lib/libNNArchPerf.so)
 
 mark_as_advanced(${OVXDRV_INCLUDE_DIRS} ${OVXDRV_LIBRARIES})
